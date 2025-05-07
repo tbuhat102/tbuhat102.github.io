871b74e5a57639aef27a5ec3fe31ce80e9292ce5
Git <ryanmoralesaz@gmail.com>
2025-05-07 03:40:02 +0000

    added navbar navigation and clipped home message

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import { useState } from "react";
 import './App.css';
 import './index.css';
 import {LoadingScreen} from "./components/LoadingScreen";
+import {Navbar} from "./components/Navbar";
+import {MobileMenu} from "./components/MobileMenu";
+import {Home} from "./components/sections/Home";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
       {!isLoaded &&  <LoadingScreen onComplete={() => setIsLoaded(true)} />}
       <div className={`min-h-screen transition-opacity duration-700 ${ isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
-      here's some content</div>
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>  
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Home />
+      </div>
     </>
   )
 }
---
+++ src/components/sections/Home.jsx
@@ -0,0 +1,10 @@
+export const Home = () => {
+  return (
+    <section id="home" className="min-h-screen flex items-center justify-center relative">
+    <div className="text-center z-10 px-4">
+      <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent gradient-text leading-right">
+        Hi, I'm Ryan!
+      </h1>
+    </div></section>
+  )
+}
