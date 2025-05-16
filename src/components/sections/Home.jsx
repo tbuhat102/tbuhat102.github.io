dafc3c70794acd4d5249922940660fb7f33ca716
Timothy Andrei Buhatin <tbuhat102@west-mec.org>
2025-05-16 14:09:55 -0700

    initial commit

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -3,7 +3,7 @@ export const Home = () => {
     <section id="home" className="min-h-screen flex items-center justify-center relative">
       <div className="text-center z-10 px-4">
         <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent gradient-text leading-right">
-          Hi, I'm Ryan!
+          Hi, I'm Andrei!
         </h1>
         <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
           I'm a full-stack developer who loves crafting, clean web applications.
