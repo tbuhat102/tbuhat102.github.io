e77a8228135ee24b324c5805921fbc6d20392dd4
tbuhat102 <tbuhat102@west-mec.org>
2025-05-20 07:51:26 -0700

    making final touches

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -6,8 +6,8 @@ export const Projects = () => {
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition">
-            <h3>Cloud Platorm</h3>
-            <p>Scalable cloud infrastructure management with automated scaling.</p>
+            <h3>Pong project</h3>
+            <p>A pong game with an AI controlled player 2.</p>
             <div className="flex flex-wrap gap-2 mb-4">
               {['tech1', 'tech2'].map((tech, key) => (
                 <span className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all" 
@@ -18,7 +18,7 @@ export const Projects = () => {
               ))}
             </div>
             <div className="flex justify-between items-center">
-              <a href="#" 
+              <a href="https://github.com/tbuhat102/Pong-revisited" 
                 className="text-blue-400 hover:text-blue-300 transition-colors my-4"
               >View Project →</a>
             </div>
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   react(),
   tailwindcss()
 ],
+base: "./",
   server: {
     host: '0.0.0.0',
     port: 5173
